e15df06e660880f7778a018a5241e6732c1f315d
Nikita Zverev <nv.zver@gmail.com>
2017-03-28 08:13:37 +0500

    Create function "setMessage"

--- static/js/chat.js
+++ static/js/chat.js
@@ -32,33 +32,34 @@
                     type: type,
                     message: $scope.message
                 },
-                success: function (res) {
-                    $scope.$apply(function () {
-                        if (res.type == 'typing') {
-                            $scope.typing = true;
-                            setTimeout(function () {
-                                $scope.sendMessage('message')
-                            }, 2000);
-                        } else {
-                            $scope.messages.push({
-                                sender: 'Server',
-                                type: 'server-message',
-                                text: res.message
-                            });
-                            $scope.message = "";
-
-                        }
-                    });
-
-                }
+                success: setMessage
             }).done(function () {
                 $scope.typing = false;
                 updateScroll();
-
             });
             $('#message').focus();
             return;
         };
+
+        function setMessage(res) {
+            $scope.$apply(function () {
+                if (res.type == 'typing') {
+                    $scope.typing = true;
+                    setTimeout(function () {
+                        $scope.sendMessage('message')
+                    }, 2000);
+                } else {
+                    $scope.messages.push({
+                        sender: 'Server',
+                        type: 'server-message',
+                        text: res.message
+                    });
+                    $scope.message = "";
+
+                }
+            });
+
+        }
 
         $scope.inputKeypress = function (keyEvent) {
             if (keyEvent.which === 13) $scope.sendMessage();
